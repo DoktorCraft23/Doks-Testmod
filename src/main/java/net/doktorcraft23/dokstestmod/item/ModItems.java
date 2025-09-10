f2307bd2ee294e2a76543e4b3a8a7a25fcca8d79
DoktorCraft23 <damiano.mazza@outlook.de>
2025-09-10 20:05:34 +0200

    Commit

--- src/main/java/net/doktorcraft23/dokstestmod/item/ModItems.java
+++ src/main/java/net/doktorcraft23/dokstestmod/item/ModItems.java
@@ -1,4 +1,20 @@
 package net.doktorcraft23.dokstestmod.item;
 
+import net.doktorcraft23.dokstestmod.DoksTestMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
 public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(DoksTestMod.MOD_ID);
+
+    public static final DeferredItem<Item> STEELINGOT = ITEMS.register("steelingot",
+            ()-> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
